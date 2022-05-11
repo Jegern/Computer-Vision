4f714824a1eb4c03d65d12836e44deaf8b664615
Jegern <severuanov2001@mail.ru>
2022-05-12 04:32:36 +0500

    Добавлен новый метод поиска пиков на гистограмме

--- ViewModels/HistogramViewModel.cs
+++ ViewModels/HistogramViewModel.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Laboratory_work_1.ViewModels.Base;
 using Laboratory_work_1.ViewModels.Store;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
+using MathNet.Numerics.Statistics;
 
 namespace Laboratory_work_1.ViewModels;
 
 public class HistogramViewModel : ViewModel
 {
+    #region Fields
+
     private int[]? _histogram;
-    private string _peaks = string.Empty;
+    private string _peakNumber = string.Empty;
 
     public ISeries[] Series { get; set; } =
     {
@@ -47,11 +49,13 @@ public class HistogramViewModel : ViewModel
         }
     }
 
-    public string Peaks
+    public string PeakNumber
     {
-        get => _peaks;
-        set => Set(ref _peaks, value);
+        get => _peakNumber;
+        set => Set(ref _peakNumber, value);
     }
+
+    #endregion
 
     /// <summary>
     /// Default constructor for code suggestions
@@ -62,86 +66,74 @@ public class HistogramViewModel : ViewModel
 
     public HistogramViewModel(ViewModelStore store) : base(store)
     {
-        if (store is not null) store.HistogramChanged += Histogram_OnChanged;
+        store.HistogramChanged += Histogram_OnChanged;
     }
 
     private void Histogram_OnChanged(int[] histogram)
     {
         Histogram = histogram;
-        var output = ZScore.StartAlgo(histogram, 30, 5.0, 0.0);
-        Peaks = $"Количество приков: {output.Signals?.Count(x => x == 1).ToString()!}";
+        var peaks = BaselinePeakFinding(histogram);
+        PeakNumber = $"Количество пиков: {peaks.Count(x => x)}";
     }
-}
 
-public class ZScoreOutput
-{
-    public List<int>? Input;
-    public List<int>? Signals;
-    public List<double>? AvgFilter;
-    public List<double>? FilteredStddev;
-}
-
-public static class ZScore
-{
-    public static ZScoreOutput StartAlgo(int[] input, int lag, double threshold, double influence)
+    private static IEnumerable<bool> DispersionPeakFinding(
+        IReadOnlyList<int> signals,
+        int lag = 30,
+        double threshold = 9.0,
+        double influence = 0.5)
     {
-        // init variables!
-        var signals = new int[input.Length];
-        var filteredY = new List<int>(input).ToArray();
-        var avgFilter = new double[input.Length];
-        var stdFilter = new double[input.Length];
+        var peaks = new bool[signals.Count];
+        var processedSignals = new double[signals.Count];
 
-        var initialWindow = new List<int>(filteredY).Skip(0).Take(lag).ToList();
-
-        avgFilter[lag - 1] = Mean(initialWindow);
-        stdFilter[lag - 1] = StdDev(initialWindow);
-
-        for (var i = lag; i < input.Length; i++)
+        for (var i = lag; i < signals.Count; i++)
         {
-            if (Math.Abs(input[i] - avgFilter[i - 1]) > threshold * stdFilter[i - 1])
+            var avg = processedSignals.Skip(i - lag).Take(lag).Mean();
+            var std = processedSignals.Skip(i - lag).Take(lag).StandardDeviation();
+            if (signals[i] - avg > threshold * std)
             {
-                signals[i] = (input[i] > avgFilter[i - 1]) ? 1 : -1;
-                filteredY[i] = (int) (influence * input[i] + (1 - influence) * filteredY[i - 1]);
+                peaks[i] = true;
+                processedSignals[i] = influence * signals[i] + (1 - influence) * processedSignals[i - 1];
             }
             else
             {
-                signals[i] = 0;
-                filteredY[i] = input[i];
+                processedSignals[i] = signals[i];
             }
-
-            // Update rolling average and deviation
-            var slidingWindow = new List<int>(filteredY).Skip(i - lag).Take(lag + 1).ToList();
-
-            avgFilter[i] = Mean(slidingWindow);
-            stdFilter[i] = StdDev(slidingWindow);
         }
 
-        // Copy to convenience class 
-        var result = new ZScoreOutput
+        return peaks;
+    }
+    
+    private static IEnumerable<bool> BaselinePeakFinding(IReadOnlyCollection<int> signals)
+    {
+        var smoothInput = new double[signals.Count];
+        for (var i = 2; i < signals.Count; i++)
+            smoothInput[i] = signals.Skip(i - 2).Take(2).Average();
+        
+        var peaks = new bool[signals.Count];
+        int? peakIndex = null;
+        double? peakValue = null;
+        var baseline = smoothInput.Average();
+
+        for (var i = 0; i < smoothInput.Length; i++)
         {
-            Input = input.ToList(),
-            AvgFilter = new List<double>(avgFilter),
-            Signals = new List<int>(signals),
-            FilteredStddev = new List<double>(stdFilter)
-        };
+            var value = smoothInput[i];
+            if (smoothInput[i] > baseline)
+            {
+                if (value <= peakValue) continue;
+                peakIndex = i;
+                peakValue = value;
+            }
+            else if (value < baseline && peakIndex != null)
+            {
+                peaks[(int) peakIndex] = true;
+                peakIndex = null;
+                peakValue = null;
+            }
+        }
 
-        return result;
-    }
+        if (peakIndex != null)
+            peaks[(int) peakIndex] = true;
 
-    private static double Mean(IEnumerable<int> list)
-    {
-        // Simple helper function! 
-        return list.Average();
-    }
-
-    private static double StdDev(IReadOnlyCollection<int> values)
-    {
-        double ret = 0;
-        if (!values.Any()) return ret;
-        var avg = values.Average();
-        var sum = values.Sum(d => Math.Pow(d - avg, 2));
-        ret = Math.Sqrt(sum / (values.Count - 1));
-
-        return ret;
+        return peaks;
     }
 }
