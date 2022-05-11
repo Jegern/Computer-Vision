6c7f4d776aaac4e63123d04af3961de36d193828
Jegern <severuanov2001@mail.ru>
2022-05-11 21:55:06 +0500

    Добавлено отображение количества пиков на гистограмме

--- ViewModels/HistogramViewModel.cs
+++ ViewModels/HistogramViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Laboratory_work_1.ViewModels.Base;
 using Laboratory_work_1.ViewModels.Store;
 using LiveChartsCore;
@@ -9,6 +11,7 @@ namespace Laboratory_work_1.ViewModels;
 public class HistogramViewModel : ViewModel
 {
     private int[]? _histogram;
+    private string _peaks = string.Empty;
 
     public ISeries[] Series { get; set; } =
     {
@@ -25,7 +28,7 @@ public class HistogramViewModel : ViewModel
             TextSize = 0,
         }
     };
-    
+
     public IEnumerable<Axis> YAxes { get; set; } = new[]
     {
         new Axis
@@ -44,6 +47,12 @@ public class HistogramViewModel : ViewModel
         }
     }
 
+    public string Peaks
+    {
+        get => _peaks;
+        set => Set(ref _peaks, value);
+    }
+
     /// <summary>
     /// Default constructor for code suggestions
     /// </summary>
@@ -53,10 +62,86 @@ public class HistogramViewModel : ViewModel
 
     public HistogramViewModel(ViewModelStore? store) : base(store)
     {
-        if (store is not null) store.HistogramChanged += histogram => Histogram = histogram;
+        if (store is not null) store.HistogramChanged += Histogram_OnChanged;
     }
 
-    #region Commands
+    private void Histogram_OnChanged(int[] histogram)
+    {
+        Histogram = histogram;
+        var output = ZScore.StartAlgo(histogram, 30, 5.0, 0.0);
+        Peaks = $"Количество приков: {output.Signals?.Count(x => x == 1).ToString()!}";
+    }
+}
 
-    #endregion
+public class ZScoreOutput
+{
+    public List<int>? Input;
+    public List<int>? Signals;
+    public List<double>? AvgFilter;
+    public List<double>? FilteredStddev;
+}
+
+public static class ZScore
+{
+    public static ZScoreOutput StartAlgo(int[] input, int lag, double threshold, double influence)
+    {
+        // init variables!
+        var signals = new int[input.Length];
+        var filteredY = new List<int>(input).ToArray();
+        var avgFilter = new double[input.Length];
+        var stdFilter = new double[input.Length];
+
+        var initialWindow = new List<int>(filteredY).Skip(0).Take(lag).ToList();
+
+        avgFilter[lag - 1] = Mean(initialWindow);
+        stdFilter[lag - 1] = StdDev(initialWindow);
+
+        for (var i = lag; i < input.Length; i++)
+        {
+            if (Math.Abs(input[i] - avgFilter[i - 1]) > threshold * stdFilter[i - 1])
+            {
+                signals[i] = (input[i] > avgFilter[i - 1]) ? 1 : -1;
+                filteredY[i] = (int) (influence * input[i] + (1 - influence) * filteredY[i - 1]);
+            }
+            else
+            {
+                signals[i] = 0;
+                filteredY[i] = input[i];
+            }
+
+            // Update rolling average and deviation
+            var slidingWindow = new List<int>(filteredY).Skip(i - lag).Take(lag + 1).ToList();
+
+            avgFilter[i] = Mean(slidingWindow);
+            stdFilter[i] = StdDev(slidingWindow);
+        }
+
+        // Copy to convenience class 
+        var result = new ZScoreOutput
+        {
+            Input = input.ToList(),
+            AvgFilter = new List<double>(avgFilter),
+            Signals = new List<int>(signals),
+            FilteredStddev = new List<double>(stdFilter)
+        };
+
+        return result;
+    }
+
+    private static double Mean(IEnumerable<int> list)
+    {
+        // Simple helper function! 
+        return list.Average();
+    }
+
+    private static double StdDev(IReadOnlyCollection<int> values)
+    {
+        double ret = 0;
+        if (!values.Any()) return ret;
+        var avg = values.Average();
+        var sum = values.Sum(d => Math.Pow(d - avg, 2));
+        ret = Math.Sqrt(sum / (values.Count - 1));
+
+        return ret;
+    }
 }
--- ViewModels/SegmentationViewModel.cs
+++ ViewModels/SegmentationViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Laboratory_work_1.Commands.Base;
 using Laboratory_work_1.ViewModels.Base;
 using Laboratory_work_1.ViewModels.Store;
+using OpenCvSharp;
 
 namespace Laboratory_work_1.ViewModels;
 
@@ -61,6 +62,10 @@ public class SegmentationViewModel : ViewModel
     {
         if (store is not null) store.HistogramChanged += histogram => Histogram = histogram;
         
+        
+        CannyMethodCommand = new Command(
+            CannyMethodCommand_OnExecuted,
+            CannyMethodCommand_CanExecute);
         PTileCommand = new Command(
             PTileCommand_OnExecuted,
             PTileCommand_CanExecute);
@@ -70,200 +75,12 @@ public class SegmentationViewModel : ViewModel
         KMeansCommand = new Command(
             KMeansCommand_OnExecuted,
             KMeansCommand_CanExecute);
-        CannyMethodCommand = new Command(
-            CannyMethodCommand_OnExecuted,
-            CannyMethodCommand_CanExecute);
+        VideoCringeCommand = new Command(
+            VideoCringeCommand_OnExecuted,
+            VideoCringeCommand_CanExecute);
     }
 
     #region Commands
-
-    #region PTileCommand
-
-    public Command? PTileCommand { get; }
-
-    private bool PTileCommand_CanExecute(object? parameter) =>
-        !PictureSize.IsEmpty &&
-        AreaPercentage is not null;
-
-    private void PTileCommand_OnExecuted(object? parameter)
-    {
-        var sum = Histogram!.Sum();
-        var remainder = sum;
-        int threshold;
-        for (threshold = 0; threshold < 256; threshold++)
-        {
-            remainder -= Histogram![threshold];
-            if (100 * remainder / sum < AreaPercentage) break;
-        }
-
-        ThresholdingSegmentation(threshold);
-        Store?.TriggerPictureBytesEvent(PictureBytes!, PictureSize);
-    }
-
-    private void ThresholdingSegmentation(int threshold)
-    {
-        for (var i = 0; i < PictureBytes!.Length; i += 4)
-        {
-            var intensity = Tools.GetPixelIntensity(PictureBytes, i);
-            Tools.SetPixel(
-                Tools.GetPixel(PictureBytes, i),
-                Tools.GetGrayPixel((byte) (intensity <= threshold ? 0 : 255)));
-        }
-    }
-
-    #endregion
-
-    #region HistogramDependentCommand
-
-    public Command? HistogramDependentCommand { get; }
-
-    private bool HistogramDependentCommand_CanExecute(object? parameter) => !PictureSize.IsEmpty;
-
-    private void HistogramDependentCommand_OnExecuted(object? parameter)
-    {
-        var threshold = 127;
-        var newThreshold = CalculateNewThreshold(threshold);
-
-        while (Math.Abs(threshold - newThreshold) != 0)
-        {
-            threshold = newThreshold;
-            newThreshold = CalculateNewThreshold(threshold);
-        }
-
-        ThresholdingSegmentation(threshold);
-        Store?.TriggerPictureBytesEvent(PictureBytes!, PictureSize);
-    }
-
-    private int CalculateNewThreshold(int threshold)
-    {
-        var leftSum = 0;
-        for (var i = 0; i < threshold; i++)
-            leftSum += (int) Histogram![i] * i;
-        var leftMean = leftSum / (int) Histogram!.Take(threshold).Sum();
-
-        var rightSum = 0;
-        for (var i = threshold; i < 256; i++)
-            rightSum += (int) Histogram![i] * i;
-        var rightMean = rightSum / (int) Histogram!.Take(new Range(threshold, 256)).Sum();
-
-        return (leftMean + rightMean) / 2;
-    }
-
-    #endregion
-
-    #region KMeansCommand
-
-    public Command? KMeansCommand { get; }
-
-    private bool KMeansCommand_CanExecute(object? parameter) =>
-        !PictureSize.IsEmpty &&
-        KNeighbours >= 2;
-
-    private void KMeansCommand_OnExecuted(object? parameter)
-    {
-        var centroids = InitializeCentroids((int) KNeighbours!);
-        var newCentroids = CalculateNewCentroids(centroids);
-        while (!centroids.SequenceEqual(newCentroids))
-        {
-            newCentroids.CopyTo(centroids, 0);
-            if (newCentroids[1] == 1) newCentroids[1] = 1;
-            newCentroids = CalculateNewCentroids(centroids);
-        }
-
-        var thresholds = new int[centroids.Length - 1];
-        for (var i = 0; i < thresholds.Length; i++)
-            thresholds[i] = (centroids[i] + centroids[i + 1]) / 2;
-
-        var palette = InitializeCentroids(centroids.Length, 0, 255);
-        for (var i = 0; i < PictureBytes!.Length; i += 4)
-        {
-            var intensity = Tools.GetPixelIntensity(PictureBytes, i);
-            var thresholdIndex = thresholds.Length;
-            for (var j = 0; j < thresholds.Length; j++)
-                if (intensity < thresholds[j])
-                {
-                    thresholdIndex = j;
-                    break;
-                }
-
-            Tools.SetPixel(
-                Tools.GetPixel(PictureBytes, i),
-                Tools.GetGrayPixel((byte) palette[thresholdIndex]));
-        }
-
-        Store?.TriggerPictureBytesEvent(PictureBytes!, PictureSize);
-    }
-
-    private int[] InitializeCentroids(int count, int? first = null, int? last = null)
-    {
-        var centroids = new int[count];
-        centroids[0] = first ?? GetHistogramMin();
-        centroids[^1] = last ?? GetHistogramMax();
-        for (var i = 0; i < centroids.Length; i++)
-            centroids[i] = centroids[0] + i * (centroids[^1] - centroids[0]) / (centroids.Length - 1);
-        return centroids;
-    }
-
-    private int GetHistogramMin()
-    {
-        var min = 0;
-        for (; min < Histogram!.Length; min++)
-            if (Histogram[min] > 0)
-                break;
-        return min;
-    }
-
-    private int GetHistogramMax()
-    {
-        var max = 255;
-        for (; max >= 0; max--)
-            if (Histogram![max] > 0)
-                break;
-        return max;
-    }
-
-    private int[] CalculateNewCentroids(IReadOnlyList<int> centroids)
-    {
-        var newCentroids = new int[centroids.Count];
-        var centroidBorders = new int[centroids.Count + 1];
-        centroidBorders[^1] = 255;
-        for (var i = 1; i < centroidBorders.Length - 1; i++)
-            centroidBorders[i] = (centroids[i] + centroids[i - 1]) / 2;
-        for (var i = 0; i < centroids.Count; i++)
-            newCentroids[i] = (int) GetMeanOfHistogram(centroidBorders[i], centroidBorders[i + 1]);
-
-        return newCentroids;
-    }
-
-    private double GetMeanOfHistogram(int start, int end)
-    {
-        var sum = 0d;
-        var counter = 0d;
-        for (var i = start; i < end; i++)
-        {
-            sum += Histogram![i] * i;
-            counter += Histogram[i];
-        }
-
-        return sum != 0 ? sum / counter : start;
-    }
-
-    public static int IndexOfClosestToTarget(IReadOnlyList<int> array, int target)
-    {
-        var closestIndex = 0;
-        var minDifference = int.MaxValue;
-        for (var i = 0; i < array.Count; i++)
-        {
-            var difference = Math.Abs(array[i] - target);
-            if (minDifference <= difference) continue;
-            minDifference = difference;
-            closestIndex = i;
-        }
-
-        return closestIndex;
-    }
-
-    #endregion
 
     #region CannyMethodCommand
 
@@ -504,5 +321,229 @@ public class SegmentationViewModel : ViewModel
         
     }
     #endregion
+
+    #region PTileCommand
+
+    public Command? PTileCommand { get; }
+
+    private bool PTileCommand_CanExecute(object? parameter) =>
+        !PictureSize.IsEmpty &&
+        AreaPercentage is not null;
+
+    private void PTileCommand_OnExecuted(object? parameter)
+    {
+        var sum = Histogram!.Sum();
+        var remainder = sum;
+        int threshold;
+        for (threshold = 0; threshold < 256; threshold++)
+        {
+            remainder -= Histogram![threshold];
+            if (100 * remainder / sum < AreaPercentage) break;
+        }
+
+        ThresholdingSegmentation(threshold);
+        Store?.TriggerPictureBytesEvent(PictureBytes!, PictureSize);
+    }
+
+    private void ThresholdingSegmentation(int threshold)
+    {
+        for (var i = 0; i < PictureBytes!.Length; i += 4)
+        {
+            var intensity = Tools.GetPixelIntensity(PictureBytes, i);
+            Tools.SetPixel(
+                Tools.GetPixel(PictureBytes, i),
+                Tools.GetGrayPixel((byte) (intensity <= threshold ? 0 : 255)));
+        }
+    }
+
+    #endregion
+
+    #region HistogramDependentCommand
+
+    public Command? HistogramDependentCommand { get; }
+
+    private bool HistogramDependentCommand_CanExecute(object? parameter) => !PictureSize.IsEmpty;
+
+    private void HistogramDependentCommand_OnExecuted(object? parameter)
+    {
+        var threshold = 127;
+        var newThreshold = CalculateNewThreshold(threshold);
+
+        while (Math.Abs(threshold - newThreshold) != 0)
+        {
+            threshold = newThreshold;
+            newThreshold = CalculateNewThreshold(threshold);
+        }
+
+        ThresholdingSegmentation(threshold);
+        Store?.TriggerPictureBytesEvent(PictureBytes!, PictureSize);
+    }
+
+    private int CalculateNewThreshold(int threshold)
+    {
+        var leftSum = 0;
+        for (var i = 0; i < threshold; i++)
+            leftSum += Histogram![i] * i;
+        var leftMean = leftSum / Histogram!.Take(threshold).Sum();
+
+        var rightSum = 0;
+        for (var i = threshold; i < 256; i++)
+            rightSum += Histogram![i] * i;
+        var rightMean = rightSum / Histogram!.Skip(threshold).Take(256).Sum();
+
+        return (leftMean + rightMean) / 2;
+    }
+
+    #endregion
+
+    #region KMeansCommand
+
+    public Command? KMeansCommand { get; }
+
+    private bool KMeansCommand_CanExecute(object? parameter) =>
+        !PictureSize.IsEmpty &&
+        KNeighbours >= 2;
+
+    private void KMeansCommand_OnExecuted(object? parameter)
+    {
+        var centroids = InitializeCentroids((int) KNeighbours!);
+        var newCentroids = CalculateNewCentroids(centroids);
+        while (!centroids.SequenceEqual(newCentroids))
+        {
+            newCentroids.CopyTo(centroids, 0);
+            if (newCentroids[1] == 1) newCentroids[1] = 1;
+            newCentroids = CalculateNewCentroids(centroids);
+        }
+
+        var thresholds = new int[centroids.Length - 1];
+        for (var i = 0; i < thresholds.Length; i++)
+            thresholds[i] = (centroids[i] + centroids[i + 1]) / 2;
+
+        var palette = InitializeCentroids(centroids.Length, 0, 255);
+        for (var i = 0; i < PictureBytes!.Length; i += 4)
+        {
+            var intensity = Tools.GetPixelIntensity(PictureBytes, i);
+            var thresholdIndex = thresholds.Length;
+            for (var j = 0; j < thresholds.Length; j++)
+                if (intensity < thresholds[j])
+                {
+                    thresholdIndex = j;
+                    break;
+                }
+
+            Tools.SetPixel(
+                Tools.GetPixel(PictureBytes, i),
+                Tools.GetGrayPixel((byte) palette[thresholdIndex]));
+        }
+
+        Store?.TriggerPictureBytesEvent(PictureBytes!, PictureSize);
+    }
+
+    private int[] InitializeCentroids(int count, int? first = null, int? last = null)
+    {
+        var centroids = new int[count];
+        centroids[0] = first ?? GetHistogramMin();
+        centroids[^1] = last ?? GetHistogramMax();
+        for (var i = 0; i < centroids.Length; i++)
+            centroids[i] = centroids[0] + i * (centroids[^1] - centroids[0]) / (centroids.Length - 1);
+        return centroids;
+    }
+
+    private int GetHistogramMin()
+    {
+        var min = 0;
+        for (; min < Histogram!.Length; min++)
+            if (Histogram[min] > 0)
+                break;
+        return min;
+    }
+
+    private int GetHistogramMax()
+    {
+        var max = 255;
+        for (; max >= 0; max--)
+            if (Histogram![max] > 0)
+                break;
+        return max;
+    }
+
+    private int[] CalculateNewCentroids(IReadOnlyList<int> centroids)
+    {
+        var newCentroids = new int[centroids.Count];
+        var centroidBorders = new int[centroids.Count + 1];
+        centroidBorders[^1] = 255;
+        for (var i = 1; i < centroidBorders.Length - 1; i++)
+            centroidBorders[i] = (centroids[i] + centroids[i - 1]) / 2;
+        for (var i = 0; i < centroids.Count; i++)
+            newCentroids[i] = (int) GetMeanOfHistogram(centroidBorders[i], centroidBorders[i + 1]);
+
+        return newCentroids;
+    }
+
+    private double GetMeanOfHistogram(int start, int end)
+    {
+        var sum = 0d;
+        var counter = 0d;
+        for (var i = start; i < end; i++)
+        {
+            sum += Histogram![i] * i;
+            counter += Histogram[i];
+        }
+
+        return sum != 0 ? sum / counter : start;
+    }
+
+    public static int IndexOfClosestToTarget(IReadOnlyList<int> array, int target)
+    {
+        var closestIndex = 0;
+        var minDifference = int.MaxValue;
+        for (var i = 0; i < array.Count; i++)
+        {
+            var difference = Math.Abs(array[i] - target);
+            if (minDifference <= difference) continue;
+            minDifference = difference;
+            closestIndex = i;
+        }
+
+        return closestIndex;
+    }
+
+    #endregion
+
+    #region VideoCringeCommand
+
+    public Command? VideoCringeCommand { get; }
+
+    private bool VideoCringeCommand_CanExecute(object? parameter) => !PictureSize.IsEmpty;
+
+    private void VideoCringeCommand_OnExecuted(object? parameter)
+    {
+        const string fileName = @"C:/Users/sever/Documents/timer_video.mp4";
+
+        var image = new Mat();
+        var window = new Window("Kek");
+        var capture = new VideoCapture(fileName);
+
+        var i = 0;
+        while (capture.IsOpened())
+        {
+            capture.Read(image);
+            if (image.Empty())
+                break;
+ 
+            i++;
+            var imgNumber = i.ToString().PadLeft(8, '0');
+ 
+            var frameImageFileName = $@"frames\image{imgNumber}.png";
+            // Cv2.ImWrite(frameImageFileName, image);
+ 
+            window.ShowImage(image);
+            if (Cv2.WaitKey(1) == 113) // Q
+                break;
+        }
+    }
+
+    #endregion
+
     #endregion
 }
